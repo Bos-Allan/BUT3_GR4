4e462edc7fa8aebd266b0d43b330a5d35874f28b
Bos-Allan <allan.bosquet@gmail.com>
2024-10-24 13:38:49 +0200

    remove ignore

--- src/test/java/com/iut/banque/test/facade/TestsBanqueManager.java
+++ src/test/java/com/iut/banque/test/facade/TestsBanqueManager.java
@@ -21,7 +21,6 @@ import com.iut.banque.facade.BanqueManager;
 @ContextConfiguration("/test/resources/TestsBanqueManager-context.xml")
 @Transactional("transactionManager")
 
-@Ignore
 public class TestsBanqueManager {
 
 	@Autowired
