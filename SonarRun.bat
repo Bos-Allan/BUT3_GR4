cec855028329ff8fd7fb1b8ac8379fa0d5cb0608
Bos-Allan <allan.bosquet@gmail.com>
2024-11-08 16:06:03 +0100

    ajout screen sonarQube fonctionnel (sans coverage) et SonarRun.bat

---
+++ SonarRun.bat
@@ -0,0 +1,5 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=BankIUT \
+  -Dsonar.projectName='BankIUT' \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_9f72eb659727cd0173eaed999149df912cad5281
