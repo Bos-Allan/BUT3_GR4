4e0d86a302ad15e343028d4e8a4697e6da5eedf6
Bos-Allan <allan.bosquet@gmail.com>
2025-01-20 22:47:25 +0100

    modif de l'encrypt pour limiter la duplication de code

--- src/main/java/com/iut/banque/controller/CreerUtilisateur.java
+++ src/main/java/com/iut/banque/controller/CreerUtilisateur.java
@@ -15,6 +15,8 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import static com.iut.banque.use.encryptMethode.encrypt;
+
 public class CreerUtilisateur extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
@@ -91,26 +93,7 @@ public class CreerUtilisateur extends ActionSupport {
 	}
 
 
-	/**
-	 * @return hashed password
-	 */
-	public static String encrypt(String userPwd) {
-		String hashedPassword = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-512");
-			byte[] encryptPass = md.digest(userPwd.getBytes(StandardCharsets.UTF_8));
 
-			StringBuilder sb = new StringBuilder();
-			for(int i=0; i< encryptPass.length ;i++){
-				sb.append(Integer.toString((encryptPass[i] & 0xff) + 0x100, 16).substring(1));
-			}
-			hashedPassword = sb.toString();
-		}
-		catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
-		}
-		return hashedPassword;
-	}
 
 	/**
 	 * @return the userPwd
